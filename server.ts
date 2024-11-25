727dc92aae538d7f6ad8c4a3862c6e8a3d79ffed
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2024-11-25 21:39:02 +0530

    add datebase setup

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
-
+import connectDB from "./src/config/db";
 
 const startServer = async () => {
-
+ // Connect database
+  await connectDB();
     const port = config.port || 3000;
 
   app.listen(port, () => {
