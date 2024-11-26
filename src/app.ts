a5dc3cac4fbf609a46a061dcd9467bc39d39ae44
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2024-11-26 15:18:23 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import createHttpError from "http-errors";
 import userRouter from "./user/userRouter";
-
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +19,7 @@ app.get("/test", (req, res) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 app.use(globalErrorHandler);
 
