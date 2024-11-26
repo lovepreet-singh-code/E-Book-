0892f9ad3cce23020a027d3f1849c1b97da383b3
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2024-11-26 18:47:52 +0530

    creat book ,cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,10 +1,70 @@
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import bookModel from "./bookModel";
+import createHttpError from "http-errors";
+import fs from "node:fs";
+//import { AuthRequest } from "../middlewares/authenticate";
 
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({});
+    const { title, genre, description } = req.body;
 
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    // 'application/pdf'
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(
+        __dirname,
+        "../../public/data/uploads",
+        fileName
+    );
+
+    try {
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: fileName,
+            folder: "book-covers",
+            format: coverImageMimeType,
+        });
+
+        const bookFileName = files.file[0].filename;
+        const bookFilePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads",
+            bookFileName
+        );
+
+        const bookFileUploadResult = await cloudinary.uploader.upload(
+            bookFilePath,
+            {
+                resource_type: "raw",
+                filename_override: bookFileName,
+                folder: "book-pdfs",
+                format: "pdf",
+            }
+        );
+       // const _req = req as AuthRequest;
+
+        const newBook = await bookModel.create({
+            title,
+            description,
+            genre,
+            author: "67455de1d62418bae868e081",
+            coverImage: uploadResult.secure_url,
+            file: bookFileUploadResult.secure_url,
+        });
+
+        // Delete temp.files
+        // todo: wrap in try catch...
+        await fs.promises.unlink(filePath);
+        await fs.promises.unlink(bookFilePath);
+
+        res.status(201).json({ id: newBook._id });
+    } catch (err) {
+        console.log(err);
+        return next(createHttpError(500, "Error while uploading the files."));
+    }
 };
 
 
