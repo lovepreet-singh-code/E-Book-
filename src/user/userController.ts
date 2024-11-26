e3d524b49fc90f23cf143a4f5a8e8652a2a3c96f
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2024-11-26 14:52:16 +0530

    update user login

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -69,23 +69,32 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(400, "All fields are required"));
   }
 
-  const user = await userModel.findOne({ email });
-  if (!user) {
-    return next(createHttpError(404, "User not found."));
-  }
+  try {
+    // Check if the user exists
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found."));
+    }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+    // Check if the password matches
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(400, "Username or password incorrect!"));
+    }
 
-  if (!isMatch) {
-    return next(createHttpError(400, "Username or password incorrect!"));
-  }
+    // Sign the JWT token
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
 
-  const token = sign({ sub: user._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
 
   res.json({ accessToken: token });
+} catch (err) {
+  
+  console.error(err); 
+  return next(createHttpError(500, "Internal Server Error"));
+}
 };
 
 export { createUser, loginUser };
