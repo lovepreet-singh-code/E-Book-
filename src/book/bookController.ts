66f2e65ddd03f94c1c3f31845b50404c79f78cf5
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2024-11-27 12:52:20 +0530

    add jwt for creat book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -4,15 +4,13 @@ import path from "node:path";
 import bookModel from "./bookModel";
 import createHttpError from "http-errors";
 import fs from "node:fs";
-//import { AuthRequest } from "../middlewares/authenticate";
-
-
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     const { title, genre, description } = req.body;
 
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-    // 'application/pdf'
+    
+    //Cover Image Da MIME Type Extract Karna:
     const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
     const fileName = files.coverImage[0].filename;
     const filePath = path.resolve(
@@ -20,7 +18,8 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         "../../public/data/uploads",
         fileName
     );
-
+ 
+    // Cloudinary Te Cover Image Upload Karna:
     try {
         const uploadResult = await cloudinary.uploader.upload(filePath, {
             filename_override: fileName,
@@ -28,13 +27,14 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
             format: coverImageMimeType,
         });
 
+        // Book PDF File Upload Karna:
         const bookFileName = files.file[0].filename;
         const bookFilePath = path.resolve(
             __dirname,
             "../../public/data/uploads",
             bookFileName
         );
-
+      
         const bookFileUploadResult = await cloudinary.uploader.upload(
             bookFilePath,
             {
@@ -44,22 +44,24 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
                 format: "pdf",
             }
         );
-       // const _req = req as AuthRequest;
-
+        
+        //New Book MongoDB Ch Create Karna
         const newBook = await bookModel.create({
             title,
             description,
             genre,
-            author: "67455de1d62418bae868e081",
+            author:"67455de1d62418bae868e081",
             coverImage: uploadResult.secure_url,
             file: bookFileUploadResult.secure_url,
         });
 
-        // Delete temp.files
-        // todo: wrap in try catch...
-        await fs.promises.unlink(filePath);
-        await fs.promises.unlink(bookFilePath);
-
+        //Temporary Files Delete Karna
+        try {
+            await fs.promises.unlink(filePath);
+            await fs.promises.unlink(bookFilePath);
+        } catch (unlinkError) {
+            console.log("Error deleting files:", unlinkError);
+        }
         res.status(201).json({ id: newBook._id });
     } catch (err) {
         console.log(err);
